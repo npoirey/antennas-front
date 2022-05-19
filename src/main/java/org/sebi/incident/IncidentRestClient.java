4578700cc3bb75c5757830fe85679964df16b721
Nicolas Poirey <nicolas.poirey@capgemini.com>
2022-05-19 14:03:43 +0200

    rest client

--- src/main/java/org/sebi/incident/IncidentRestClient.java
+++ src/main/java/org/sebi/incident/IncidentRestClient.java
@@ -12,5 +12,5 @@ import java.util.List;
 public interface IncidentRestClient {
     @GET
     @Path("/incidents")
-    List<Incident> getIncidents(@QueryParam("APIKEY") String apiKey);
+    List<Incident> getIncidents(@QueryParam("api_key") String apiKey);
 }
