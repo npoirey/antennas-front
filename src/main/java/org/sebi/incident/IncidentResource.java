94814d3f056d0bef986c12903e5b40d7efcbe163
Nicolas Poirey <nicolas.poirey@capgemini.com>
2022-05-19 13:15:16 +0200

    rest client

--- src/main/java/org/sebi/incident/IncidentResource.java
+++ src/main/java/org/sebi/incident/IncidentResource.java
@@ -1,15 +1,23 @@
 package org.sebi.incident;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
 import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
-@Path("rest/incidents")
+@Path("incidents")
 public class IncidentResource {
-    
+    @RestClient
+    IncidentRestClient incidentRestClient;
+
+    @ConfigProperty(name = "APIKEY", defaultValue = "secret")
+    String apiKey;
+
     @GET
     public List<Incident> getIncidents(){
-       return Incident.listAll();
+       return incidentRestClient.getIncidents(apiKey);
     } 
 }
---
+++ src/main/java/org/sebi/incident/IncidentRestClient.java
@@ -0,0 +1,16 @@
+package org.sebi.incident;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import java.util.List;
+
+@Path("/rest")
+@RegisterRestClient(configKey = "incident-api")
+public interface IncidentRestClient {
+    @GET
+    @Path("/incidents")
+    List<Incident> getIncidents(@QueryParam("APIKEY") String apiKey);
+}
